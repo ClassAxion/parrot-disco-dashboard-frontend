056cfff03ac2eaad037753fcb83528d7e4d61b26
Szymon <>
2021-07-05 21:40:57 +0200

    Working env

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -35,7 +35,7 @@ export default defineComponent({
         }),
     },
     mounted() {
-        if (!process.env.NO_DISCO && !this.isConnected)
+        if (!process.env.VUE_APP_NO_DISCO && !this.isConnected)
             window.location.href = '/';
     },
 });
