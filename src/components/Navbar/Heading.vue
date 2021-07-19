5b841a55e41e202c903ecd2e0005dd1e0b4cf19b
Damian (ClassAxion) <damian@techseo.pl>
2021-07-19 18:35:13 +0200

    heading implemented

--- src/components/Navbar/Heading.vue
+++ src/components/Navbar/Heading.vue
@@ -1,8 +1,40 @@
 <template>
     <div class="heading">
-        <span>360°</span>
+        <span>{{ yawDegress }}°</span>
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { mapState } from 'vuex';
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        yawAngle: number;
+    }
+}
+
+export default defineComponent({
+    computed: {
+        ...mapState({
+            yawAngle: (state: any) => {
+                return state.orientation.yaw;
+            },
+        }),
+        yawDegress() {
+            let degress = Number(
+                this.variableMap(this.yawAngle, -180, 180, 0, 360).toFixed(0),
+            );
+
+            return degress;
+        },
+    },
+    methods: {
+        variableMap: (value, inMin, inMax, outMin, outMax) =>
+            ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin,
+    },
+});
+</script>
 
 <style lang="scss" scoped>
 .heading {
