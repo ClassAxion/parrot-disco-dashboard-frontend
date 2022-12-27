f7587101bf69aebfc4c969bc424badc36317b645
Damian <damian@techseo.pl>
2022-12-27 15:18:57 +0100

    feat: initial settings page (#13)

---
+++ src/components/Background/Background.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="image">
+        <img :src="setImage" />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { mapState } from 'vuex';
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        stream: any;
+    }
+}
+
+const Image = require('@/assets/img/background.png');
+
+export default defineComponent({
+    computed: {
+        ...mapState({
+            stream: (state: any) => state.stream,
+        }),
+        setImage() {
+            return Image;
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    img {
+        min-width: 100%;
+        min-height: 100%;
+        margin: 0;
+    }
+}
+</style>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,3 +1,311 @@
 <template>
-    <h1>Settings</h1>
+    <div class="settings">
+        <Background />
+        <form>
+            <h1>Settings</h1>
+            <router-link to="/dashboard" class="back"
+                >Back to flight</router-link
+            >
+
+            <div class="inputWrapper">
+                <p class="title">Return to home type</p>
+                <div class="select" @click="rth.showValues = !rth.showValues">
+                    <span>{{ rth.value || 'UNKNOWN' }}</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        version="1.1"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            fill="#3d3d3d"
+                            d="M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M7,10L12,15L17,10H7Z"
+                        />
+                    </svg>
+                    <ul class="values" v-if="rth.showValues">
+                        <li
+                            v-for="value in availableRTHValues"
+                            @click="setRTH(value)"
+                            :key="value"
+                        >
+                            {{ value }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div v-if="rth.value == 'PILOT'">
+                <div class="inputWrapper">
+                    <p class="title">Pilot latitude</p>
+                    <input type="text" placeholder="53.34925" />
+                </div>
+
+                <div class="inputWrapper">
+                    <p class="title">Pilot longitude</p>
+                    <input type="text" placeholder="17.64046" />
+                </div>
+
+                <div class="inputWrapper">
+                    <p class="title">Pilot altitude <small>[meters]</small></p>
+                    <input type="text" placeholder="167" />
+                </div>
+            </div>
+
+            <button type="button" class="save">Submit settings</button>
+
+            <p class="poland">Made with <span>❤</span> in Poland</p>
+        </form>
+        <div class="overlay"></div>
+    </div>
 </template>
+
+<script lang="ts">
+import Background from '@/components/Background/Background.vue';
+import { defineComponent } from 'vue';
+
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        rth: {
+            values: string[];
+            value: string;
+            showValues: boolean;
+        };
+    }
+}
+
+export default defineComponent({
+    components: {
+        Background,
+    },
+    data() {
+        return {
+            rth: {
+                values: ['TAKEOFF', 'PILOT'],
+                value: 'UNKNOWN',
+                showValues: false,
+            },
+        };
+    },
+    computed: {
+        availableRTHValues() {
+            return this.rth.values;
+        },
+    },
+    methods: {
+        setRTH(value: string) {
+            this.rth.value = value;
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.settings {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    form {
+        background-color: white;
+        width: 50vw;
+        height: 75vh;
+        border-radius: 5px;
+        padding: 2rem;
+        z-index: 1;
+        position: relative;
+        @media (max-width: 550px) {
+            width: 90%;
+        }
+        h1 {
+            display: flex;
+            flex-direction: column;
+            letter-spacing: 1px;
+            margin-bottom: 1rem;
+            font-size: 2rem;
+        }
+        p {
+            margin: 0.5rem 0;
+            font-size: 0.9rem;
+            letter-spacing: 0.75px;
+        }
+    }
+
+    .overlay {
+        background-color: rgba(#3d3d3d, 0.7);
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 0;
+    }
+
+    .inputWrapper {
+        width: 70%;
+        margin: 0 auto;
+        margin-top: 1rem;
+        .input {
+            position: relative;
+        }
+        input {
+            all: unset;
+            box-sizing: border-box;
+            width: 70%;
+            height: 40px;
+            padding-left: 1rem;
+            padding-right: 2.5rem;
+            letter-spacing: 1px;
+            font-weight: 600;
+            color: #3d3d3d;
+            font-size: 0.8rem;
+            border-radius: 5px;
+            text-align: left;
+            border: 2px solid #3d3d3d;
+            transition: 0.1s all ease-in-out;
+            &:focus {
+                background: rgba(#3d3d3d, 0.1);
+            }
+        }
+
+        .select {
+            box-sizing: border-box;
+            width: 70%;
+            margin: 0 auto;
+            height: 40px;
+            padding-left: 1rem;
+            padding-right: 2.5rem;
+            letter-spacing: 1px;
+            font-weight: 400;
+            font-size: 0.8rem;
+            border-radius: 5px;
+            text-align: left;
+            border: 2px solid #3d3d3d;
+            transition: 0.1s all ease-in-out;
+            margin-top: 0.2rem;
+            display: flex;
+            align-items: center;
+            position: relative;
+            cursor: pointer;
+            user-select: none;
+            svg {
+                position: absolute;
+                right: 10px;
+            }
+            .values {
+                background-color: white;
+                position: absolute;
+                bottom: calc(-100% - 60px);
+                left: 0;
+                list-style: none;
+                width: 100%;
+                border: 2px solid rgba(#3d3d3d, 0.5);
+                border-radius: 5px;
+                li {
+                    height: 50px;
+                    display: flex;
+                    align-items: center;
+                    letter-spacing: 1px;
+                    padding: 0 1rem;
+                    color: #3d3d3d;
+                    &:hover {
+                        background: rgba(#3d3d3d, 0.1);
+                    }
+                }
+            }
+            @media (max-width: 550px) {
+                width: 90%;
+            }
+        }
+
+        .disabled {
+            border: 2px solid #979797;
+            background: rgba(#979797, 0.05);
+            color: #979797;
+        }
+
+        svg {
+            position: absolute;
+            top: 50%;
+            right: 10px;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
+        .title {
+            padding: 0;
+            margin: 0;
+            text-align: center;
+            font-size: 1rem;
+            margin-top: 0.2rem;
+        }
+        @media (max-width: 550px) {
+            width: 90%;
+        }
+    }
+
+    .back {
+        all: unset;
+        height: 25px;
+        padding: 3px;
+        margin: 0 auto;
+        background-color: #3d3d3d;
+        border-radius: 5px;
+        color: white;
+        font-weight: 600;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        border: 2px solid #3d3d3d;
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+        user-select: none;
+        margin-bottom: 0.5rem;
+        &:hover {
+            color: #3d3d3d;
+            background: white;
+        }
+        @media (max-width: 550px) {
+            width: 90%;
+        }
+    }
+    .save {
+        all: unset;
+        height: 25px;
+        padding: 3px;
+        margin: 1rem auto;
+        background-color: #3d3d3d;
+        border-radius: 5px;
+        color: white;
+        font-weight: 600;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        border: 2px solid #3d3d3d;
+        transition: all 0.2s ease-in-out;
+        cursor: pointer;
+        user-select: none;
+        margin-bottom: 0.5rem;
+        &:hover {
+            color: #3d3d3d;
+            background: white;
+        }
+        @media (max-width: 550px) {
+            width: 90%;
+        }
+    }
+    .poland {
+        width: 100%;
+        margin: 0;
+        font-weight: 600;
+        color: #979797;
+        position: absolute;
+        bottom: 8px;
+        font-size: 0.7rem;
+        left: 50%;
+        transform: translateX(-50%);
+        span {
+            color: #e74c3c;
+        }
+    }
+}
+</style>
